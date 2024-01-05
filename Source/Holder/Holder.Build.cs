a1ab7bbf31a76669e53c21ef4cdc07819691a87c
FA77\flavi <flavioalex19@gmail.com>
2024-01-04 21:29:59 -0300

    MyCharacter Script

--- Source/Holder/Holder.Build.cs
+++ Source/Holder/Holder.Build.cs
@@ -8,7 +8,7 @@ public class Holder : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
