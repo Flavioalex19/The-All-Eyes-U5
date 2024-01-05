7c05f229f8e3e74a6bef2db33a76b949a368b771
FA77\flavi <flavioalex19@gmail.com>
2024-01-04 21:09:58 -0300

    First Commit - Part 9

---
+++ Source/Holder/Holder.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Holder.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Holder, "Holder" );
