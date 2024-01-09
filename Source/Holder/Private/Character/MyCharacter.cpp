d16f70092be4365d479fb97856b97d9ce40a3ef6
FA77\flavi <flavioalex19@gmail.com>
2024-01-08 23:02:54 -0300

    Input mapping, action input and movement

--- Source/Holder/Private/Character/MyCharacter.cpp
+++ Source/Holder/Private/Character/MyCharacter.cpp
@@ -2,6 +2,12 @@
 
 
 #include "Character/MyCharacter.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AMyCharacter::AMyCharacter()
@@ -9,12 +15,35 @@ AMyCharacter::AMyCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = CameraArmLenght;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("View Camera"));
+	ViewCamera->SetupAttachment(CameraBoom);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//APlayerController* PlayerController = Cast<PlayerController>(GetController());
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			
+			Subsystem->AddMappingContext(MyMapping, 0);
+		}
+	}
 	
 }
 
--- Source/Holder/Public/Character/MyCharacter.h
+++ Source/Holder/Public/Character/MyCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "MyCharacter.generated.h"
 
 UCLASS()
@@ -19,6 +20,20 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	//Inputs
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Input)
+	class UInputMappingContext* MyMapping;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Input)
+	class UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= Input)
+	class UInputAction* LookAction;
+
+	//Camera Variables
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	class USpringArmComponent* CameraBoom;
+	class UCameraComponent* ViewCamera;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	float CameraArmLenght = 300.f;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
